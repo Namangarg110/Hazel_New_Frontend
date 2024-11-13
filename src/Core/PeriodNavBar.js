8db9b82e9d930b8c4a7f50e47dbd2173651efaa2
PiyushGoyal02 <goyalp3542@gmail.com>
2024-11-13 15:07:56 +0530

    I Have create ProvidrNavBar.js

--- src/Core/PeriodNavBar.js
+++ src/Core/PeriodNavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../CSS_Code/PeriodNavBarCSS.css"
 import { useNavigate } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
@@ -12,11 +13,23 @@ function PeriodNavBar(){
     return (
         <div>
             <div className="LeftArrowIcon-ScheduleText LeftLogo-Text">
+
+                <div className="ProviderNavBarDiv">
                     <div onClick={OneStepBackHandler} className="LeftArrowIcon">
                         <FiArrowLeft size={38} style={{ cursor: 'pointer' }} />
                     </div>
-                    <h1>Period 11/1</h1>
+                    <div className="PeriodDatetext-TimeText">
+                        <h1>Period 11/1-11/30 2024</h1>
+                        <p>2 times a week - Tue, Thu</p>
+                    </div>
                 </div>
+
+                <div className="BookOneSixtyText-BookOneSixtyButton">
+                    <span>Book 160 appointments for students with 8 providers</span>
+                    <button className="book-OneSixty-appointment-Button">Book 160 appointments</button>
+                </div>
+
+            </div>
         </div>
     )
 }
